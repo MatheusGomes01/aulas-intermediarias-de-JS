41a9bdd46a3bd76288f2cdb66b7d388fb0c09f03
Matheus Gomes <matheus01gomes01ferreira2001@gmail.com>
2018-01-22 10:30:06 -0200

    Aula 1 do EcmanScript

--- teste.php
+++ teste.php
@@ -1,46 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame Remove this you use the htaccess-->
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome">
-
-
-	<title>Index</title>
-	<meta name="description" content="">
-	<meta name="author" content="Paulo">
-	<meta name="viewport" content="width=device-width initial-scale=1.0">
-	<!-- Replace favicon ico & apple-touch-icon.png in the root domain and delete-->
-	<link rel="shortcut" href="/favicon.ico">
-	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
-
+	<title></title>
 </head>
 <body>
-	<div>
-	<div><label>Nome:</label>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-	<label>Sobreome:</label><div>
-	<div class="bnt bnt-mini"><input type="text" name="tbNome" />&nbsp;
-	<input type="text" name="tbSobrenome" /></div>
-	
-	
-	<p></p>
-	<div><label>idade:</label></div>
-	<input type="number" name="nIdade" />
-	<p></p>
-	
 
-	<div><label>Data de Nascimento</label></div>
-	<input type="number" name="nIdade" />
-	<p></p>
-	<input type="file" name="ficheiro" />
-	<p></p>
-
-
-	<button id="btnEnviar">Enviar</button>
-</div>
-<script src="script.js" type="text/javascript">
-
-
+<script>
+	function teste(param = 'es2015'){
+		return param;
+	}
+	console.log(teste());
 </script>
+
+
 </body>
 </html>
