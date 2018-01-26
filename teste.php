8308c30ab9f0e902a9e30a2e55a667073d4646b8
Matheus Gomes <matheus01gomes01ferreira2001@gmail.com>
2018-01-26 11:09:45 -0200

    Aula 9, Spread, quebrando elementos de array

--- teste.php
+++ teste.php
@@ -6,14 +6,13 @@
 <body>
 
 <script>
-function mediaAluno(p1, p2, p3){
-	return ((p1 + p2 + p3) / 3).toFixed(2);
-}
 
-var notasAluno = [7.7, 7.3, 6.1, 0.0];
-console.log(mediaAluno.apply(null, notasAluno));
-console.log(mediaAluno(...notasAluno));
 
+var notasAlunos = [6.8, 7.9, 8.2];
+var turmaES6 = notasAlunos.concat([6.7, 8.9, 9.1]);
+var turmaES5 = [...notasAlunos, 6.7, 8.9, 9.1]
+console.log(turmaES5);
+console.log(turmaES6);
  
 </script>
 
