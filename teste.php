330f8768fd32f8ccfaeffcb15480732725488b8d
Matheus Gomes <matheus01gomes01ferreira2001@gmail.com>
2018-01-26 11:41:49 -0200

    Aula 10,  Arrow function de diferentes formas, uma delas citada, mas não enserida é sem os return

--- teste.php
+++ teste.php
@@ -6,13 +6,51 @@
 <body>
 
 <script>
-//Rest agrupando elementos.
+// Arrow function
+var estoque = [
+{nome: 'Resma de Papel A4', preco : 19.90, qtde: 300, fragil: false},
+{nome: 'Monitor LG 29 IPS', preco : 1699, qtde: 15, fragil: true},
+{nome: 'Ovos Brancos - 30 Unid.', preco : 18.99, qtde: 21, fragil: true},
+{nome: 'Pneu Pirelli Scorpion Aro 19', preco : 1027.34, qtde: 16, fragil: false},
+{nome: 'Apple Ipad Pro 32gb - Tela 12,9', preco : 3999, qtde: 17, fragil: true},
+];
+const qtdeProdutosCarosEFrageis = estoque
+.filter(function(item){
+	return item.preco >= 1000;
+})
+.filter(function(item){
+	return item.fragil;
+})
+.map(function(item){
+	return item.qtde;
+})
+.reduce(function  (total, qtde){
+	return total + qtde;
+});
 
-function media(...notas){
-	var total = notas.reduce((soma,nota) => soma + nota);
-	return total / notas.length;
-}
-console.log(media(7.3, 7.7, 6.0));
+console.log(qtdeProdutosCarosEFrageis);
+</script>
+
+<script>
+// Arrow function
+var estoque = [
+{nome: 'Resma de Papel A4', preco : 19.90, qtde: 300, fragil: false},
+{nome: 'Monitor LG 29 IPS', preco : 1699, qtde: 15, fragil: true},
+{nome: 'Ovos Brancos - 30 Unid.', preco : 18.99, qtde: 21, fragil: true},
+{nome: 'Pneu Pirelli Scorpion Aro 19', preco : 1027.34, qtde: 16, fragil: false},
+{nome: 'Apple Ipad Pro 32gb - Tela 12,9', preco : 3999, qtde: 17, fragil: true},
+];
+const isPrecoAlto = item => item.preco >= 1000;
+const isFragil = item => item.fragil;
+const somatorio = (total, qtde) => total + qtde;
+
+const qtdeProdutosCarosEFrageis = estoque
+.filter(isPrecoAlto)
+.filter(isFragil)
+.map(item => item.qtde)
+.reduce(somatorio);
+
+console.log(qtdeProdutosCarosEFrageis);
 </script>
 
 
